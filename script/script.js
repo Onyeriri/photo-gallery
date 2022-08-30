d7cb12be9d8aaeb6632895a8078b6612c93984d4
Onyeriri Victor <viktorchinonso@gmail.com>
2022-08-30 15:53:52 +0100

    Completed secod section feature

--- script/script.js
+++ script/script.js
@@ -1,13 +1,17 @@
+// First card section variables
 let descriptionElement = document.getElementById('description');
-
 let closeElement = document.getElementById('close');
-
 let BTNLetter = document.getElementById('card_two_btn');
-
 let openLetter = document.getElementById('card_one_btn');
 
-const toggleDescription = () => {
+// Second card section variables
+let secondDescriptionOne = document.getElementById('second_description_one');
+let secondClose = document.getElementById('second_close');
+let secondBTNOne = document.getElementById('second_one_btn');
+let secondBTNTwo = document.getElementById('second_two_btn');
 
+// First section function declaration
+const toggleDescription = () => {
   if (descriptionElement.style.display === 'block') {
     descriptionElement.style.display = 'none';
 
@@ -21,7 +25,6 @@ const toggleDescription = () => {
 }
 
 const closeDescription = () => {
-
   if (closeElement.style.display === 'block') {
     closeElement.style.display = 'none';
     BTNLetter.innerText = 'Open description';
@@ -33,4 +36,27 @@ const closeDescription = () => {
 
 }
 
-console.log(descriptionElement.style.display);
+// second card section function declaration
+const secondToggleDescription = () => {
+  if (secondDescriptionOne.style.display === 'block') {
+    secondDescriptionOne.style.display = 'none';
+
+    secondBTNOne.innerText = 'Open description';
+  } else {
+    secondDescriptionOne.style.display = 'block';
+
+    secondBTNOne.innerText = 'Close description';
+  }
+
+};
+
+const SecondCloseDescription = () => {
+  if (secondClose.style.display === 'block') {
+    secondClose.style.display = 'none';
+    secondBTNTwo.innerText = 'Open description';
+
+  } else {
+    secondClose.style.display = 'block';
+    secondBTNTwo.innerText = 'Close description';
+  }
+};
