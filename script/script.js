ce8ebcd36f0f17a66a708c3fde5b9b66ca3ec4c8
Onyeriri Victor <viktorchinonso@gmail.com>
2022-09-01 02:04:55 +0100

    Added third section to photo gallery project

--- script/script.js
+++ script/script.js
@@ -10,6 +10,13 @@ let secondClose = document.getElementById('second_close');
 let secondBTNOne = document.getElementById('second_one_btn');
 let secondBTNTwo = document.getElementById('second_two_btn');
 
+// third card section variables
+let thirdDescriptionOne = document.getElementById('third_description_one');
+let thirdClose = document.getElementById('third_close');
+let thirdBTNOne = document.getElementById('third_one_btn');
+let thirdBTNTwo = document.getElementById('third_two_btn');
+
+
 // First section function declaration
 const toggleDescription = () => {
   if (descriptionElement.style.display === 'block') {
@@ -60,3 +67,29 @@ const SecondCloseDescription = () => {
     secondBTNTwo.innerText = 'Close description';
   }
 };
+
+// third section function declaration
+const thirdToggleDescription = () => {
+  if (thirdDescriptionOne.style.display === 'block') {
+    thirdDescriptionOne.style.display = 'none';
+
+    thirdBTNOne.innerText = 'Open description';
+  } else {
+    thirdDescriptionOne.style.display = 'block';
+
+    thirdBTNOne.innerText = 'Close description'
+  }
+
+}
+
+const thirdCloseDescription = () => {
+  if (thirdClose.style.display === 'block') {
+    thirdClose.style.display = 'none';
+    thirdBTNTwo.innerText = 'Open description';
+
+  } else {
+    thirdClose.style.display = 'block';
+    thirdBTNTwo.innerText = 'Close description'
+  }
+
+}
